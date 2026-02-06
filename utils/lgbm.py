b89268374eb3248f23b15954ac12f22e676ef11c
shaurya <work.shauryasingru@gmail.com>
2026-02-06 19:13:42 +0530

    utils folder + updated requirements.txt

---
+++ utils/lgbm.py
@@ -0,0 +1,31 @@
+import joblib
+import lightgbm
+import time
+from utils.preprocess import load_preprocess
+
+def load_model(model_path):
+    start = time.time()
+    model = joblib.load(model_path)
+    duration = time.time() - start
+    print(f'Model took {duration:.2f}s to load')
+    return model
+
+def predict(model, X):
+    start = time.time()
+    y_preds = model.predict_proba(X)[:,1]
+    duration = time.time() - start
+    print(f'Model took {duration:.2f}s to predict')
+    return y_preds
+
+
+if __name__ == '__main__':
+
+    import numpy as np
+
+    model = load_model('model/lgbm.pkl')
+
+    X, y = load_preprocess('data/')
+
+    y_preds = predict(model, X)
+
+    print(np.where(y_preds[:10,1] > 0.5, 1, 0))
