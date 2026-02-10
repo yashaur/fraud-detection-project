c5e2ae29b7499951f03792545f8dcd8a9f19145b
shaurya <work.shauryasingru@gmail.com>
2026-02-10 15:45:04 +0530

    Threshold slider value now persists across pages

---
+++ pages/about_us.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from utils.init import init_session_vars
+
+init_session_vars()
+
+st.write("Nothing for now")
--- pages/threshold_slider.py
+++ pages/threshold_slider.py
@@ -1,4 +1,5 @@
 # IMPORTING LIBRARIES
+from utils.init import init_session_vars
 from utils.data import load_preprocess
 from utils.model import load_model, predict
 from utils.precision_recall import precision_recall, precision_recall_array
@@ -6,40 +7,39 @@ from utils.charts import create_pr_doughnuts, create_pr_chart
 import streamlit as st
 import matplotlib.pyplot as plt
 
-
-### INITIALISING SESSION STATE VARIABLES ###
-
-# Initialising X and y
-if 'X' not in st.session_state or 'y' not in st.session_state:
-    st.session_state['X'], st.session_state['y'] = load_preprocess()
-X, y = st.session_state['X'], st.session_state['y']
-
-# Initiliasing the model
-if 'model' not in st.session_state:
-    st.session_state['model'] = load_model()
-model = st.session_state['model']
-
-# Initialising predicted probabilities
-if 'y_probs' not in st.session_state:
-    st.session_state['y_probs'] = predict(model, X)
-y_probs = st.session_state['y_probs']
-
-# Initialising the threshold
-if 'threshold' not in st.session_state:
-    threshold = 0.5
-else:
-    threshold = st.session_state['threshold'] / 100
-
-# Initialising the precision-recall array at different thresholds
-if 'pr_data' not in st.session_state:
-    st.session_state['pr_data'] = precision_recall_array(X, y, model, y_probs)
-pr_data = st.session_state['pr_data']
-
+init_session_vars()
 
 ### STREAMLIT PAGE CODE ###
 
 # Title of the page
 st.title("Threshold Slider")
+
+# Initialising the threshold
+
+st.button(
+            label = "Reset to 50%",
+            on_click = lambda: st.session_state.update({'threshold': 50, 'threshold_slider': 50})
+        )
+
+
+threshold_int = st.slider(
+                        label = "Choose threshold (%):",
+                        min_value = 0,
+                        max_value = 100,
+                        step = 1,
+                        key = 'threshold_slider',
+                        value = st.session_state['threshold'],
+                        on_change = lambda: st.session_state.update({'threshold': st.session_state['threshold_slider']})
+)
+
+threshold = threshold_int / 100
+
+# ### INITIALISING SESSION STATE VARIABLES ###
+
+X, y = st.session_state['X'], st.session_state['y']
+model = st.session_state['model']
+y_probs = st.session_state['y_probs']
+pr_data = st.session_state['pr_data']
 
 # Creating the precision and recall doughnut charts
 precision, recall = precision_recall(thresh = threshold, y_probs = y_probs, y_test = y)
@@ -51,4 +51,3 @@ pr_chart = create_pr_chart(pr_data, precision, recall)
 st.pyplot(pr_chart)
 
 # Slider to control threshold
-st.slider(label = "Choose threshold (%):", min_value = 0, max_value = 100, step = 1, value = 50, key = 'threshold')
